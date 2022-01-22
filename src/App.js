5dae181f7ca4c2311a5eaa40f5f24bb71d5ee134
prabhudevv <prabhudev.vatnal55@gmail.com>
2022-01-22 16:17:57 +0530

    Removed unwanted import

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 
 import "./App.css";
-import "toastr2/dist/toastr.min.css";
 
 import alert from "./components/HotToaster/HotToaster";
 import Navigation from "./components/Navigation/Navigation";
